8f7f9eeaa650e15232673b38d68b69cef9f1c2ce
KhasanAlimov <hasanalimov021@gmail.com>
2021-05-16 12:38:54 +0600

    Template

---
+++ server.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const expressHbs = require("express-handlebars");
+const hbs = require("hbs");
+const routes = require('./routes.js');
+
+// Настройки HandleBars
+app.engine("hbs", expressHbs(
+    {
+        layoutsDir: "views/layouts", 
+        defaultLayout: "layout",
+        extname: "hbs"
+    }
+));
+
+app.set("view engine", "hbs");
+hbs.registerPartials(__dirname + "/views/partials");
+
+app.use(express.static(__dirname + '/public'));
+
+routes(app);
+
+
+try {
+  app.listen(process.env.PORT, () => {
+    console.log(`Сервер запущен в порту ${process.env.PORT}!`);
+  });
+}catch (e) {
+  console.log(e);
+}
+
